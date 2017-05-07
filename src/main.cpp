cbbfe191092d187a875c9ff4dcd32b249643c0ba
Tim Hawes <me@timhawes.com>
2017-05-07 15:38:39 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,149 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+
+extern "C" {
+#include "user_interface.h"
+}
+
+#include "config.h"
+
+#define MAX_PAYLOAD_LENGTH 10
+
+char my_id[40];
+char mqtt_id[24];
+
+WiFiClient client;
+PubSubClient mqtt(client);
+
+unsigned long last_read = 0;
+
+void callback(const MQTT::Publish& pub)
+{
+  mqtt.publish("meta/mqtt-agents/reply", my_id);
+}
+
+float read_current()
+{
+  const int reading_count = 70;
+  const int adc_samples = 4;
+
+  int values[reading_count];
+  int min_val = 1024 * adc_samples;
+  int max_val = 0;
+
+  for (int a=0; a<reading_count; a++) {
+    values[a] = 0;
+    for (int i = 0; i < adc_samples; i++) {
+      values[a] += analogRead(A0);
+    }
+  }
+
+  for (int i=0; i<reading_count; i++) {
+    if (values[i] > max_val) {
+      max_val = values[i];
+    }
+    if (values[i] < min_val) {
+      min_val = values[i];
+    }
+  }
+
+  float diff = (float) (max_val - min_val) / adc_samples;
+  float current = fudge_factor * amps_per_volt * diff / 1024.0 / 2.828;
+  return current;
+}
+
+void setup()
+{
+  snprintf(my_id, sizeof(my_id), hostname_template, ESP.getChipId());
+  snprintf(mqtt_id, sizeof(mqtt_id), "%08x", ESP.getChipId());
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.print(my_id);
+  Serial.print(" ");
+  Serial.println(ESP.getSketchMD5());
+
+  if (ota_enabled) {
+    Serial.println("Enabling OTA updates");
+    ArduinoOTA.setPort(8266);
+    ArduinoOTA.setHostname(my_id);
+    if (strlen(ota_password) > 0) {
+      Serial.print("OTA password: ");
+      Serial.println(ota_password);
+      ArduinoOTA.setPassword(ota_password);
+    }
+    ArduinoOTA.onStart([]() {
+      Serial.println("OTA Start");
+    });
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nOTA End");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("OTA Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("OTA Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
+  }
+
+  wifi_station_set_hostname(my_id);
+  WiFi.mode(WIFI_STA);
+
+  mqtt.set_server(mqtt_server, mqtt_port);
+  mqtt.set_callback(callback);
+}
+
+void loop()
+{
+
+  if (ota_enabled) {
+    ArduinoOTA.handle();
+  }
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    Serial.println("...");
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1);
+    }
+    Serial.println("WiFi connected");
+  }
+
+  if (!mqtt.connected()) {
+    if (mqtt.connect(mqtt_id)) {
+      Serial.println("MQTT connected");
+      mqtt.subscribe("meta/mqtt-agents/poll");
+    } else {
+      Serial.println("MQTT connection failed");
+      delay(2000);
+      return;
+    }
+  }
+
+  mqtt.loop();
+
+  if (last_read == 0 || millis() - last_read > read_interval) {
+    last_read = millis();
+    char mqtt_payload[MAX_PAYLOAD_LENGTH];
+    float current = read_current();
+    dtostrf(current, 0, 2, mqtt_payload);
+    Serial.print(mqtt_topic);
+    Serial.print(" ");
+    Serial.println(mqtt_payload);
+    mqtt.publish(mqtt_topic, (const uint8_t*)mqtt_payload, strlen(mqtt_payload), true);
+  }
+
+}
